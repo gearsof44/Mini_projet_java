dbbe465b74c0bc4b424715a2dcb77cb6c3b4db67
yoanRAPULEG <yoan.kyun@gmail.com>
2017-09-12 09:43:16 +0200

    Article et matelas a air intanciés

---
+++ LocationMaterielMedical/src/Article.java
@@ -0,0 +1,46 @@
+
+public class Article {
+	private int ref;
+	private String marque;
+	private String modele;
+	private double prixLocationJour;
+	private int nbStock;
+	public Article(int ref, String marque, String modele, double prixLocationJour, int nbStock) {
+		super();
+		this.ref = ref;
+		this.marque = marque;
+		this.modele = modele;
+		this.prixLocationJour = prixLocationJour;
+		this.nbStock = nbStock;
+	}
+	public int getRef() {
+		return ref;
+	}
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+	public String getMarque() {
+		return marque;
+	}
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+	public String getModele() {
+		return modele;
+	}
+	public void setModele(String modele) {
+		this.modele = modele;
+	}
+	public double getPrixLocationJour() {
+		return prixLocationJour;
+	}
+	public void setPrixLocationJour(double prixLocationJour) {
+		this.prixLocationJour = prixLocationJour;
+	}
+	public int getNbStock() {
+		return nbStock;
+	}
+	public void setNbStock(int nbStock) {
+		this.nbStock = nbStock;
+	}
+}
---
+++ LocationMaterielMedical/src/MatelasAAir.java
@@ -0,0 +1,9 @@
+
+public class MatelasAAir extends Article{
+
+	private String longueur;
+	private String largeur;
+	private String hauteur;
+	private double 
+
+}
