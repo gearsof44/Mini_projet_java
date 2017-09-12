598a68d4d9f3d0d4d715e0c8be0c823d1434d57c
yoanRAPULEG <yoan.kyun@gmail.com>
2017-09-12 09:59:22 +0200

    Fauteil LitMecanise SouleveMalade TableEtLit initialisés

---
+++ LocationMaterielMedical/src/SouleveMalade.java
@@ -0,0 +1,24 @@
+
+public class SouleveMalade extends Article{
+	private int capaciteLevage;
+	private int degrePivotFleau;
+	public SouleveMalade(int ref, String marque, String modele, double prixLocationJour, int nbStock,
+			int capaciteLevage, int degrePivotFleau) {
+		super(ref, marque, modele, prixLocationJour, nbStock);
+		this.capaciteLevage = capaciteLevage;
+		this.degrePivotFleau = degrePivotFleau;
+	}
+	public int getCapaciteLevage() {
+		return capaciteLevage;
+	}
+	public void setCapaciteLevage(int capaciteLevage) {
+		this.capaciteLevage = capaciteLevage;
+	}
+	public int getDegrePivotFleau() {
+		return degrePivotFleau;
+	}
+	public void setDegrePivotFleau(int degrePivotFleau) {
+		this.degrePivotFleau = degrePivotFleau;
+	}
+
+}
---
+++ LocationMaterielMedical/src/TableEtLit.java
@@ -0,0 +1,39 @@
+
+public class TableEtLit extends Article{
+	private int poidsMax;
+	private String longueur;
+	private String largeur;
+	private String hauteur;
+	public TableEtLit(int ref, String marque, String modele, double prixLocationJour, int nbStock, int poidsMax,
+			String longueur, String largeur, String hauteur) {
+		super(ref, marque, modele, prixLocationJour, nbStock);
+		this.poidsMax = poidsMax;
+		this.longueur = longueur;
+		this.largeur = largeur;
+		this.hauteur = hauteur;
+	}
+	public int getPoidsMax() {
+		return poidsMax;
+	}
+	public void setPoidsMax(int poidsMax) {
+		this.poidsMax = poidsMax;
+	}
+	public String getLongueur() {
+		return longueur;
+	}
+	public void setLongueur(String longueur) {
+		this.longueur = longueur;
+	}
+	public String getLargeur() {
+		return largeur;
+	}
+	public void setLargeur(String largeur) {
+		this.largeur = largeur;
+	}
+	public String getHauteur() {
+		return hauteur;
+	}
+	public void setHauteur(String hauteur) {
+		this.hauteur = hauteur;
+	}
+}
