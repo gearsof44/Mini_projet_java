2f25fd1703cbcf7f0739495aa52920a78202b313
Gearsof44 <Gearsof44officiel@gmail.com>
2017-09-12 10:32:07 +0200

    commit before merge

--- LocationMaterielMedical/src/TableEtLit.java
+++ LocationMaterielMedical/src/TableEtLit.java
@@ -1,9 +1,25 @@
 
+/**
+ * @author visiteur-info11
+ *
+ */
 public class TableEtLit extends Article{
 	private int poidsMax;
 	private String longueur;
 	private String largeur;
 	private String hauteur;
+	
+	/**
+	 * @param ref
+	 * @param marque
+	 * @param modele
+	 * @param prixLocationJour
+	 * @param nbStock
+	 * @param poidsMax
+	 * @param longueur
+	 * @param largeur
+	 * @param hauteur
+	 */
 	public TableEtLit(int ref, String marque, String modele, double prixLocationJour, int nbStock, int poidsMax,
 			String longueur, String largeur, String hauteur) {
 		super(ref, marque, modele, prixLocationJour, nbStock);
